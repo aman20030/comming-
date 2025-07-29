fdc004722e3a067cb036f100548f3471632a6dbe
aman20030 <2022pcecaaman004@poornima.org>
2025-07-29 19:02:38 +0530

    first commit

---
+++ src/ComingSoon.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ComingSoon.css';
+
+const ComingSoon = () => {
+  return (
+    <div className="container">
+      <div className="top-left">#Coming Soon</div>
+      <div className="middle-left">
+        Stay tuned for exciting updates on fleet management!
+      </div>
+      <div className="top-right">
+        <span className="text-3d">Page Not Found</span>
+      </div>
+    </div>
+  );
+};
+
+export default ComingSoon;
